845cd64c998b571d7fc10bf5d5e43959d6646023
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-19 21:53:51 -0400

    removed js comments code

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ var menu = document.querySelector('.menu');
 var burgerBtn = document.querySelector('.hamburger-btn');
 var menuItem = document.querySelector('.hamburger-menu');
 
-//Open menu when clicking the hamburger btn
-/*burgerBtn.onClick = function() {
 
-    this.classList.toggle("open");
-};*/
 
 function openMenu(){
     if(menuItem.classList.add('show-menu')) {
@@ -20,10 +16,6 @@ function openMenu(){
             
         }
     }
-    //document.burgerBtn.setAttribute('aria-expanded', 'true');
-    //this.getAttribute('aria-expanded', 'true');
-    
-    
     burgerBtn.addEventListener("click", xMenu)
     
 }
