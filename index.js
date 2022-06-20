1738eb772dd76dcf16b8dd76e70960caead08320
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-19 22:05:32 -0400

    click outside of menu function

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var burgerBtn = document.querySelector('.hamburger-btn');
 var menuItem = document.querySelector('.hamburger-menu');
 
 
-
 function openMenu(){
     if(menuItem.classList.add('show-menu')) {
         burgerBtn.ariaExpanded = true
@@ -12,16 +11,18 @@ function openMenu(){
         function xMenu() {
             menuItem.classList.remove('show-menu')
             //this.getAttribute('aria-expanded', 'false');
-            burgerBtn.ariaExpanded = false
-            
+            burgerBtn.ariaExpanded = false   
         }
     }
-    burgerBtn.addEventListener("click", xMenu)
-    
-}
+    burgerBtn.addEventListener("click", xMenu)  
 
+    document.body.onclick = function(e) {
+        if(!menu.contains(e.target)) {
+            xMenu()
+        }
+    }
+}
 burgerBtn.addEventListener("click", openMenu);
 
-    
 
 
